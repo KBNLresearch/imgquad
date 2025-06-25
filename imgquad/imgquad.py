3de59aae2af71079a8dbcd320b47ecc5608b0c35
bitsgalore <johan.vanderknijff@kb.nl>
2025-06-25 17:23:10 +0200

    bumped version string

--- imgquad/imgquad.py
+++ imgquad/imgquad.py
@@ -20,7 +20,7 @@ from . import properties
 from . import schematron
 from . import shared
 
-__version__ = "0.1.0a1"
+__version__ = "0.1.0a2"
 
 # Create parser
 parser = argparse.ArgumentParser(description="IMaGe QUality Assessment for Digitisation batches")
